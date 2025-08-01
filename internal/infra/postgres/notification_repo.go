b888e2fc7f1df30b2b0630dcaed311333376d8fc
katelinlis <katelinlis1999@gmail.com>
2025-08-01 08:17:59 +0300

    fix

--- internal/infra/postgres/notification_repo.go
+++ internal/infra/postgres/notification_repo.go
@@ -18,7 +18,7 @@ func NewNotificationRepository(db *sql.DB) repository.NotificationRepository {
 
 func (r *notificationRepository) Create(ctx context.Context, n *model.MessageCreatedEvent) error {
 	_, err := r.db.ExecContext(ctx, `
-		INSERT INTO notifications (user_id, message, is_read,from_id,type, created_at)
+		INSERT INTO notifications (user_id, message, read,from_id,type, created_at)
 		VALUES ($1, $2, false,$3,$4, $5)
 	`, n.ReceiverID, n.Content, n.FromID, n.Type, n.CreatedAt)
 	return err
@@ -26,7 +26,7 @@ func (r *notificationRepository) Create(ctx context.Context, n *model.MessageCre
 
 func (r *notificationRepository) FindByUserID(context context.Context, id int) ([]model.MessageCreatedEvent, error) {
 	rows, err := r.db.QueryContext(context, `
-		SELECT id, user_id, message, is_read,from_id,type, created_at
+		SELECT id, user_id, message, read,from_id,type, created_at
 		FROM notifications
 		WHERE user_id = $1
 	`, id)
@@ -50,7 +50,7 @@ func (r *notificationRepository) FindByUserID(context context.Context, id int) (
 func (r *notificationRepository) MarkAsRead(ctx context.Context, id int) error {
 	_, err := r.db.ExecContext(ctx, `
 		UPDATE notifications
-		SET is_read = true
+		SET read = true
 		WHERE id = $1
 	`, id)
 	return err
